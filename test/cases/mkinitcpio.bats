archlinux/mkinitcpio

commit 2ca86680756bed59c169545d3f98e4c6c0f6814d

add -p to fix reproducibilty

mkinitcpio
@@ -289,6 +289,9 @@ build_uki(){
     local -i offset=0
     OBJCOPYARGS=()
 
+    # reproducibility, preserve dates of used files
+    OBJCOPYARG+=(-p)
+
     msg "Creating unified kernel image: '%s'" "$out"
 
     if [[ -z "$uefistub" ]]; then

commit 5e47003bef795739859500ffa0957e5fa397f3c5

tests: Add tests to check for reproducible builds
Also fixes a typo.

Fixes: 2ca86680756bed59c169545d3f98e4c6c0f6814d
Signed-off-by: Morten Linderud <morten@linderud.pw>

.gitlab-ci.yml
@@ -11,7 +11,7 @@ stages:
         # https://gitlab.archlinux.org/archlinux/archlinux-keyring/-/issues/4
         - echo -e "\e[0Ksection_start:`date +%s`:pacman[collapsed=true]\r\e[0KInstalling dependencies"
         - pacman -Sy --needed --noconfirm archlinux-keyring
-        - pacman --noconfirm -Syu --needed bash-bats bash-bats-assert bash-bats-support diffutils git jq kcov lzop make shellcheck
+        - pacman --noconfirm -Syu --needed bash-bats bash-bats-assert bash-bats-support diffutils git jq kcov lzop make shellcheck linux
         - echo -e "\e[0Ksection_end:`date +%s`:pacman\r\e[0K"
 
 check:
@@ -28,6 +28,8 @@ test:
     artifacts:
         reports:
             junit: bats-report.xml
+    tags:
+        - vm
 
 coverage:
     stage: check

mkinitcpio
@@ -299,7 +299,7 @@ build_uki() {
     OBJCOPYARGS=()
 
     # reproducibility, preserve dates of used files
-    OBJCOPYARG+=(-p)
+    OBJCOPYARGS+=(-p)
 
     msg "Creating unified kernel image: '%s'" "$out"
 

test/cases/mkinitcpio.bats
@@ -1,0 +1,40 @@
+#!/usr/bin/env bats
+# SPDX-License-Identifier: GPL-2.0-only
+
+@test "test reproducible builds for initramfs" {
+    local tmpdir
+    tmpdir=$(mktemp -d --tmpdir="$BATS_RUN_TMPDIR" "${BATS_TEST_NAME}.XXXXXX")
+
+    echo "HOOKS=(base)" >> "$tmpdir/mkinitcpio.conf"
+
+    run ./mkinitcpio \
+        -c "$tmpdir/mkinitcpio.conf" \
+        -g "$tmpdir/initramfs-1.img"
+
+    run ./mkinitcpio \
+        -c "$tmpdir/mkinitcpio.conf" \
+        -g "$tmpdir/initramfs-2.img"
+
+    run cmp "$tmpdir/initramfs-1.img" "$tmpdir/initramfs-2.img"
+    (( status == 0 ))
+}
+
+
+@test "test reproducible builds for uki" {
+    local tmpdir
+    tmpdir=$(mktemp -d --tmpdir="$BATS_RUN_TMPDIR" "${BATS_TEST_NAME}.XXXXXX")
+
+    echo "HOOKS=(base)" >> "$tmpdir/mkinitcpio.conf"
+
+    ./mkinitcpio \
+        -c "$tmpdir/mkinitcpio.conf" \
+        --uki "$tmpdir/uki-1.efi"
+
+    ./mkinitcpio \
+        -c "$tmpdir/mkinitcpio.conf" \
+        --uki "$tmpdir/uki-2.efi"
+
+    sha256sum "$tmpdir/uki-1.efi" "$tmpdir/uki-2.efi"
+    run cmp "$tmpdir/uki-1.efi" "$tmpdir/uki-2.efi"
+    (( status == 0 ))
+}