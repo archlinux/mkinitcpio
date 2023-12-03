c29dbd498fb33f41c725a1088c72ee4cadf86164
nl6720 <nl6720@gmail.com>
2023-12-03 13:09:32 +0200

    Update tools/git-contrib.sh
    Make the script executable and pull in the latest changes from upstream.
    
    See https://github.com/systemd/systemd/commit/b242d2dec9bad65142fab66180b68a78cdb0f570

--- tools/git-contrib.sh
+++ tools/git-contrib.sh
@@ -4,7 +4,12 @@
 set -eu
 
 tag="$(git describe --abbrev=0 --match 'v[0-9]*')"
-git log --pretty=tformat:%aN -s "${tag}.." |
-    sed 's/ / /g; s/--/-/g; s/.*/\0,/' |
+(
+    # authors
+    git log --pretty=tformat:%aN -s "${tag}.."
+    # Co-authors (drop empty line and mail addresses)
+    git log --pretty='tformat:%(trailers:key=Co-authored-by,valueonly)' -s "${tag}.." | sed -e '/^[[:space:]]*$/ d' | sed -e 's/ <.*@.*>$//'
+) |
+    sed 's/ / /g; s/--/-/g; s/.*/\0,/' |
     sort -u | tr '\n' ' ' | sed -e "s/^/Contributions from: /g" -e "s/,\s*$/\n/g" | fold -w 72 -s |
     sed -e "s/^/        /g" -e "s/\s*$//g"
